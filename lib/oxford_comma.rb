4b6d67e4ae16be6a43ac722efbf8ae637e92f332
Pancho Labrador <plabradorjr@gmail.com>
2019-10-12 14:31:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_array = array.last
+    new_last_array = "and #{last_array}"
+    array.pop
+    array << new_last_array
+    array.join(", ")
+  end
 end
